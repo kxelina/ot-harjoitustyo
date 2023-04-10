28e724e8b354a0b4d71f43b0c4ad0e1070cbe4ec
kxelina <120955082+kxelina@users.noreply.github.com>
2023-04-10 19:10:01 +0300

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -11,27 +11,23 @@ def coverage(ctx):
 @task(coverage)
 def coverage_report(ctx):
     ctx.run("coverage html", pty=True)
-<<<<<<< HEAD
+
 
 @task
 def format(ctx):  # pylint: disable=redefined-builtin
     ctx.run("autopep8 --in-place --recursive src", pty=True)
-=======
+
     
 @task
 def test(ctx):
     ctx.run("pytest src", pty=True)
 
->>>>>>> 72eb2cc3fcb7796b496cbc8ac08949d812478352
+
 
 @task
 def lint(ctx):
     ctx.run("pylint src", pty=True)
-<<<<<<< HEAD
-=======
 
 
-@task
-def format(ctx):  # pylint: disable=redefined-builtin
-    ctx.run("autopep8 --in-place --recursive src", pty=True)
->>>>>>> 72eb2cc3fcb7796b496cbc8ac08949d812478352
+
+
